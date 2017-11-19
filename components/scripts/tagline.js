f14c96e6241d54da070bba7a35e45b708aa3a50b
KEIKO UEDA <keiko77mel@gmail.com>
2017-11-19 23:30:33 +1100

    Added CoffeeScript files

---
+++ components/scripts/tagline.js
@@ -0,0 +1,11 @@
+var fill;
+
+(fill = (function(_this) {
+  return function(item) {
+    return $('.tagline').append("" + {
+      item: item
+    });
+  };
+})(this))('The most creative minds in Art');
+
+fill;
